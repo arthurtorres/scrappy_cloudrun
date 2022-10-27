a7814516d58e4771ed32b95aed0ab3db6dc33f85
arthur <arthurincluir@gmail.com>
2022-10-27 15:02:52 -0300

    deleting code that dosent work anymore

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ from pipes import Template
 from numpy import number
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
-from helpers.general_tools import dtProcessamento_option,df_to_gcs,remove_garbage,clean_dataframe_titulos
+from helpers.general_tools import dtProcessamento_option,df_to_gcs
 from helpers.hackernews_tools import get_all_links_hackerrank,data_to_dataframe_hackerank,transform
-from helpers.googlenews_tools import get_dataframe,clean_df
-from helpers.inforchannel_tools import create_dataframe
 from helpers.prensa_tools import create_dataframe_prensa
 from datetime import datetime
 app = Flask(__name__)
@@ -39,49 +37,6 @@ class getTheHackerNews(Resource):
             result = "Fracasso"
 
         return jsonify({"result": result})
-class getGoogleNews(Resource) :
-    def post(self) :
-
-        try :
-            data = request.get_json()
-            tema = data["tema"]
-            pesquisa = data["pesquisa"]
-            tempo = data["tempo"]
-            data = datetime.date(datetime.today())
-            df =get_dataframe(pesquisa,tempo)
-            df = clean_df(df)
-            tamanho_original = df.shape[0]
-            df = clean_dataframe_titulos(df)
-            tamanho_final = df.shape[0]
-            qtd_removidas = tamanho_original-tamanho_final
-            df["tema_principal"] = tema
-            df =dtProcessamento_option(df)
-            df_to_gcs("portifolio-arthur",f"std-raw/scrappy/GN-{pesquisa}/{tempo}/data-collect-{tema}-{data}.parquet",df)
-            result = "Sucesso"
-            quantidade = df.shape[0]
-        except :
-            result = "Fracasso"
-            quantidade = -1
-            qtd_removidas = -1
-        return jsonify({"result": result ,
-                        "Quantidade" : quantidade,
-                        "qtd_removidas" : qtd_removidas})
-
-
-class removeGarbage(Resource) :
-    def post(self) :
-            data = request.get_json()
-            titulo = data["titulo"]
-            result,count_del,count_geral = remove_garbage("4sys-data",titulo)
-            if result == "Achou arquivo com esse nome" :
-                count = count_del
-                
-            else :
-                count = count_geral
-
-            return jsonify({"result": result,
-                            "Quantidade" : count} )
-
 
 class getPrensa(Resource) :
     def post(self) :
@@ -107,8 +62,6 @@ class getPrensa(Resource) :
                         "Quantidade" : quantidade})
 
 api.add_resource(getTheHackerNews, '/postTheHackerNews')
-api.add_resource(getGoogleNews, '/getGoogleNews')
-api.add_resource(removeGarbage, '/garbageRemove')
 api.add_resource(getPrensa, '/getPrensa')
 
 if __name__ == "__main__":
--- helpers/general_tools.py
+++ helpers/general_tools.py
@@ -40,39 +40,4 @@ def df_to_gcs(bucket_gcs,filepath,dataframe):
     blob = buckets.blob(filepath)
     blob.upload_from_string(io_file.getvalue(), content_type='application/octet-stream')
 
-def remove_garbage(bucket_gcs,filename) :
-    bucket = client.get_bucket(bucket_gcs)
-    # list all objects in the directory
-    directory = "trusted-inteligencia_mercado"
-    blobs = bucket.list_blobs(prefix=directory)
-            
-    tem_arquivo = "Não achou nenhum arquivo com esse nome"
-    count_geral = 0
-    count_deletados = 0
-    for blob in blobs :
-        count_geral += 1
-        if filename in blob.name :
-            print(blob.name)
-            blob.delete()
-            tem_arquivo = "Achou arquivo com esse nome"
-            count_deletados += 1
-
-    return tem_arquivo, count_deletados,count_geral    
         
-def clean_dataframe_titulos(df) :
-
-    PALAVRAS_GERAIS = ["inscrição","palestra","webninário","inscrever","Forum",
-            "livro","vagas","mortes","cursos","morte","SEGS","covid","COVID","Galaxy",
-            "Haddad","Veja","veja","Bolsonaro","MBA","dicas","dica","Ciência Fundamental",
-            "pesquisa","diversidade","IR","feriado","IBGE","Meet the Expert"
-             "saque","confira","evento","documentário"]
-
-
-    df['flagCol'] = np.where(df.titulos.str.contains('|'.join(PALAVRAS_GERAIS)),1,0)
-    mask_geral = df['flagCol'] == 1
-    df = df.drop(columns = "flagCol")
-    df =df[~mask_geral]
-    
-    mask_eleicoes = df["titulos"].str.contains("%") & df["titulos"].str.contains("Big Data")
-    df =df[~mask_eleicoes]    
-    return df 
